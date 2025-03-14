a89eaaedf7d4d49a4fc24fe09da91c2b6af901f3
oryx3422 <blagodarenko1@mail.ru>
2025-03-14 17:38:03 +0300

    1-4

--- client.py
+++ client.py
@@ -1,15 +1,24 @@
 import socket
-from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
+
+print("КЛИЕНТ: Попытка соединения с сервером на адресе localhost:9090...")
 sock.connect(('localhost', 9090))
+print("КЛИЕНТ: Соединение с сервером установлено.")
 
-msg = input("Input_string: ")
-#msg = "Hi!"
+msg = input("Введите строку для отправки серверу: ")
+
+print("КЛИЕНТ: Отправка данных серверу...")
 sock.send(msg.encode())
+print("КЛИЕНТ: Данные успешно отправлены серверу.")
 
+print("КЛИЕНТ: Ожидание ответа от сервера...")
 data = sock.recv(1024)
-sock.close()
+print("КЛИЕНТ: Данные получены от сервера.")
 
-print(data.decode())
+sock.close()
+print("КЛИЕНТ: Соединение с сервером закрыто.")
+
+print("Ответ от сервера:", data.decode())
+
--- server.py
+++ server.py
@@ -1,20 +1,34 @@
 import socket
 
+print("СЕРВЕР: Запуск сервера...")
+
 sock = socket.socket()
-sock.bind(('', 9090))
+sock.bind(('localhost', 9090))
+print("СЕРВЕР: Сокет привязан к порту 9090.")
+
 sock.listen(0)
+print("СЕРВЕР: Начало прослушивания входящих подключений.")
+
 conn, addr = sock.accept()
-print(addr)
+print(f"СЕРВЕР: Подключился клиент с адресом {addr}.")
 
 msg = ''
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    data = conn.recv(1024)
+    if not data:
+        print("СЕРВЕР: Клиент прекратил отправку данных или отключился.")
+        break
+    print(f"СЕРВЕР: Получена порция данных от клиента: {data.decode()}")
+    msg += data.decode()
+    conn.send(data)
+    print("СЕРВЕР: Порция данных отправлена обратно клиенту.")
 
+print("СЕРВЕР: Полное сообщение, полученное от клиента:")
 print(msg)
 
 conn.close()
+print("СЕРВЕР: Клиент отключился.")
+
+sock.close()
+print("СЕРВЕР: Остановка сервера. Работа завершена.")
